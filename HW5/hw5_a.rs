93dfe4e0037f034679ba084d5e105c05d8c0e7e1
shamant024 <mallikarjunappa.s@northeastern.edu>
2025-02-15 20:36:06 -0500

    hw5 updated

--- HW5/hw5_a.rs
+++ HW5/hw5_a.rs
@@ -1,34 +1,28 @@
 use std::thread;
 use std::time::Duration;
 
-fn main(){
-	
-	let handle1 = thread::spawn(move || {
-		for i in 0..10 {
-			println!("T1 {}", i);
-			thread::sleep(Duration::from_secs(1));
+fn main() {
+    let handle1 = thread::spawn(move || {
+        let handle2 = thread::spawn(move || {
+            let handle3 = thread::spawn(move || {
+                for i in 0..10 {
+                    println!("T3 {}", i);
+                    thread::sleep(Duration::from_secs(1));
+                }
+            });
+            for i in 0..10 {
+                println!("T2 {}", i);
+                thread::sleep(Duration::from_secs(1));
+            }
+            handle3.join().unwrap();
+        });
+        for i in 0..10 {
+            println!("T1 {}", i);
+            thread::sleep(Duration::from_secs(1));
+        }
+        handle2.join().unwrap();
+    });
 
-		}
-		
-		let handle2 = thread::spawn(move || {
-			for i in 0..10 {
-				println!("T2 {}", i);
-				thread::sleep(Duration::from_secs(1));
-			}
-			
-			let handle3 = thread::spawn(move || {
-				for i in 0..10 {
-					println!("T3 {}", i);
-					thread::sleep(Duration::from_secs(1));
-				}
-			});
-			
-			handle3.join().unwrap();
-		});
-		
-		handle2.join().unwrap();
-	});
-	
-	handle1.join().unwrap();
-	std::process::exit(0);
+    handle1.join().unwrap();
+    std::process::exit(0);
 }
